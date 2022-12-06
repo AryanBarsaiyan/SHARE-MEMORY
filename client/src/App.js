edfb6c0798fd0c6cd0ccb50d503bb521de3f68f6
Aryan Barsaiyan <82722871+AryanBarsaiyan@users.noreply.github.com>
2022-12-06 16:46:52 +0530

    Final

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Layout, Image, Typography } from "antd";
+import Logo from "./images/Instaverse.png";
+import Home from './components/Home';
+import styles from './styles';
+
+const { Title } = Typography;
+const { Header, Footer } = Layout;
+
+const App = () => {
+    return (
+        <Layout style={styles.layout}>
+            <Header style={styles.header}>
+                <Image  style={styles.image} preview={false} src={Logo} width={45} />
+                &nbsp;
+                <Title  style={styles.title}>Share Memory</Title>
+            </Header>
+            <Home />
+            <Footer  style={styles.footer}>copyright @ 2022 Share Memory</Footer>
+        </Layout>
+    )
+}
+
+export default App;
